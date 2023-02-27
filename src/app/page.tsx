002a9726d102c1935514a568c4888ec05567f0a7
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-02-27 00:27:43 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,46 @@
-import Image from 'next/image'
+import Papa from 'papaparse'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+type Link = {
+  label: string
+  url: string
+}
+
+async function getLinks() {
+  const res = await fetch(
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vTjUZebFwj89SiMeHNSCBCXOKci1bGNtL3VfQND2DeaB9vM0dCJr0rrVSiNMqStHAZJBifJpuVHXsTC/pub?gid=0&output=csv'
+  )
+  const data = await res.text()
+  const dataParsed = await new Promise<Link[]>((resolve, reject) => {
+    Papa.parse<Link>(data, {
+      header: true,
+      complete: (result) => resolve(result.data),
+      error: reject,
+    })
+  })
+
+  return dataParsed
+}
+
+export default async function Home() {
+  const links = await getLinks()
+  console.log(links)
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main
+      className={`text-zinc-100 grid place-content-center min-h-screen ${inter.className}`}
+    >
+      <h1 className='text-2xl font-semibold'>User1</h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.url}>
+            <a target='_blank' rel='noreferrer' href={link.url}>
+              {link.label}
+            </a>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
